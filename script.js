13d9e8f681570da80123f3ba1d9e5bea2dc3b9a5
avagagner <46656654+avagagner@users.noreply.github.com>
2022-05-02 18:29:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,64 @@
-var mymap = L.map('mapid').setView([38.63775922635261, -90.28368046396037], 13);
-//https://a.tile.openstreetmap.org/{z}/{x}/{y}.png
-//'http://a.tile.stamen.com/toner/{z}/{x}/{y}.png'
-//https://wiki.openstreetmap.org/wiki/Tile_servers
-L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	subdomains: 'abcd',
-	minZoom: 1,
-	maxZoom: 16,
-	ext: 'jpg'
-}).addTo(mymap);
+require(["esri/views/MapView",
+          "esri/WebMap","esri/widgets/Legend", ], (MapView, WebMap, Legend) => {
+        /************************************************************
+         * Creates a new WebMap instance. A WebMap must reference
+         * a PortalItem ID that represents a WebMap saved to
+         * arcgis.com or an on-premise portal.
+         *
+         * To load a WebMap from an on-premise portal, set the portal
+         * url with esriConfig.portalUrl.
+         ************************************************************/
+        const webmap = new WebMap({
+          portalItem: {
+            // autocasts as new PortalItem()
+            id: "d74d693132ce4e8abb617b1b4b74a825"
+          }
+        });
 
-var marker = L.marker([38.63672876590212, -90.24674730564345]).addTo(mymap);
+        /************************************************************
+         * Set the WebMap instance to the map property in a MapView.
+         ************************************************************/
+        const view = new MapView({
+          map: webmap,
+          container: "viewDiv"
+        });
+   
+   view.when(() => {
+          // get the first layer in the collection of operational layers in the WebMap
+          // when the resources in the MapView have loaded.
+          const featureLayer = webmap.layers.getItemAt(1);
 
-var marker2 = L.marker([38.63816176328897, -90.25104626536422]).addTo(mymap);
+          const legend = new Legend({
+            view: view,
+            layerInfos: [
+              {
+                layer:  featureLayer,
+                title: "City Size"
+    
+              }
+            ]
+          });
 
-var marker3 = L.marker([38.661539699331904, -90.30894447950988]).addTo(mymap);
+          // Add widget to the bottom right corner of the view
+          view.ui.add(legend, "bottom-right");
+        });
+    view.when(() => {
+          // get the first layer in the collection of operational layers in the WebMap
+          // when the resources in the MapView have loaded.
+          const featureLayer = webmap.layers.getItemAt(0);
 
-var marker4 = L.marker([38.61714841986633, -90.27511168787689]).addTo(mymap);
+          const legend = new Legend({
+            view: view,
+            layerInfos: [
+              {
+                layer:  featureLayer,
+                title: "April Housing Prices"
+    
+              }
+            ]
+          });
 
-var marker5 = L.marker([38.64751484426265, -90.2744901289432]).addTo(mymap);
-
-
-
-marker.bindPopup("<b>Scottish Arms</b>.").openPopup();
-marker2.bindPopup("<b>Scarlett's Wine Bar</b>.").openPopup();
-marker3.bindPopup("<b>Mi Ranchito</b>.").openPopup();
-marker4.bindPopup("<b>Lorenzo's Trattoria</b>.").openPopup();
-marker4.bindPopup("<b>2Shae Cafe</b>.").openPopup();
+          // Add widget to the bottom right corner of the view
+          view.ui.add(legend, "top-right");
+        });
+      });
